a03534dbdcc5b0b563cdaba5e9f72db445e8c099
Ashwini Biradar <ashwinichambole@gmail.com>
2024-09-28 17:29:12 +0200

    added files

---
+++ src/pck1/Test1.java
@@ -0,0 +1,12 @@
+package pck1;
+
+import java.util.ArrayList;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList <String> al= new ArrayList<>();
+        al.add("ashwini");
+        al.add("as");
+        System.out.println(al);
+    }
+}
